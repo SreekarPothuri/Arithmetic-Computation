0f052f8c505c27cdeebb2a9fd8f1061651f5de01
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-12 20:24:29 +0530

    UC-6:Added

--- Arithmetic_Calculations.sh
+++ Arithmetic_Calculations.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "Welcome to Arithmetic computation and sorting problem"
 
@@ -10,4 +10,19 @@ pb1=$(( $a + ($b * $c) ))
 pb2=$(( ($a * $b) + $c ))
 pb3=$(( $c + ($a / $b) ))
 pb4=$(( ($a % $b) + $c ))
+
+declare -A Calculations
+declare -a ArrayofResults
+store_results()
+{
+	Calculations[$1]=$2
+}
+
+store_results 1 $pb1
+store_results 2 $pb2
+store_results 3 $pb3
+store_results 4 $pb4
+
+echo "Key : ${!Calculations[@]} ---- Value : ${Calculations[@]}"
+
 
